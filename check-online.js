fe60171a289a7c30fe52289a4eded6cfcd3a856b
gfaceless <giggle2016@gmail.com>
2016-04-15 20:39:16 +0800

    use rxjs instead

--- check-online.js
+++ check-online.js
@@ -1,64 +1,34 @@
 'use strict';
 
-
 var Promise = require('bluebird')
 var request = require('request')
 
-Promise.config({
+/*Promise.config({
   cancellation: true
-})
+})*/
 
-var interval = 60 * 1000
 let onlineStr = '<span class="success label">online</span>'
 
 
-module.exports = function check(url) {
+module.exports = function check(url, meta) {
   return new Promise(function(resolve, reject, onCancel) {
-    // store pending requests:
-    var requests = [];
 
-    function go() {
-      var r = request(url, function(error, response, body) {
+    request(url, function(error, response, body) {
+      if (error) { /*ommit error for now*/ }
 
-        if (error) { /*ommit error for now*/ }
+      if (!error && response.statusCode == 200) {
 
-        pop(requests, r)
-
-        if (!error && response.statusCode == 200) {
-
-          let online = body.includes(onlineStr);
-          if (online) {
-            // TODO: cancel remaining requests
-            console.log('checked, ONLINE', new Date().toLocaleTimeString())
-
-            resolve()
-            clearInterval(timer)
-
-            // cancel pending requests
-            requests.forEach(function(r) {
-              r.abort();
-            })
-
-          } else {
-            console.log('checked, not online', new Date().toLocaleTimeString())
-
-          }
+        let online = body.includes(onlineStr);
+        if (online) {
+          // TODO: cancel remaining requests
+          console.log(`checked, ONLINE ${meta}`, new Date().toLocaleTimeString())
+        } else {
+          console.log('checked, not online', new Date().toLocaleTimeString())
         }
-      })
-
-      requests.push(r)
-    }
-    var timer = setInterval(go, interval)
-    go();
-    onCancel(function() {
-      console.log('cancelled!')
-      clearInterval(timer)
+        resolve(online);
+      }
     })
-  })
-}
 
+  });
 
-function pop(arr, item) {
-  var idx = arr.indexOf(item)
-  if (idx > -1) arr.splice(idx, 1)
 }
--- index.js
+++ index.js
@@ -1,40 +1,100 @@
 'use strict';
 const notifier = require('node-notifier');
-var Promise = require('bluebird');
+const ONE_MIN = 60 * 1000;
 
-var check = require('./check-online')
-
-// String
-// notifier.notify('Message');
-
+var Rx = require("rx");
 var urls
 
-urls = [
-  'http://poe.trade/search/imokimikimaham',
-  'http://poe.trade/search/atoamanoarinaz',
-  // 'http://poe.trade/search/siterehusitara', // online
+var carts = [
+  {name: "wind of change", url: 'http://poe.trade/search/imokimikimaham'},
+  {name: "enlighten <= 50 chaos", url: 'http://poe.trade/search/atoamanoarinaz'},
+  // {name: "some always online item(test only)", url: 'http://poe.trade/search/siterehusitara'}, // online
+  {name: '(buy it!)Maligaro > 45 mul', url: 'http://poe.trade/search/watakenikikomi'} 
 ]
-// url = 'http://poe.trade/search/kononihuramduz';
 
-var checkers = urls.map(function(url) {
-  return check(url)
+var starters = {status: undefined}
+
+var checkOnline = require("./check-online")
+
+
+var streams = carts
+.map(function(item) {
+  var latestIdx = -1;
+
+  return Rx.Observable.timer(0, ONE_MIN)
+  
+  .flatMap(function(idx) {
+    return checkOnline(item.url, item.name)
+    .then(function(status) {
+      
+      /*status: online, offline, sold*/
+      if(latestIdx < idx) latestIdx = idx;
+      return {idx, status, item}
+    })
+  })
+  // eliminate those requests first but comes later
+  .filter(function(result) {
+    // console.log('in filter', result);
+    let latest = result.idx === latestIdx;
+    if(!latest) console.log('VERY RARE that previous comes later', result.idx, latestIdx);
+    return latest;
+  })
+  .distinctUntilChanged(function(result) {
+    return result.status
+  })
+  // if we want `combineLatest` to work as soon as we get a response, 
+  // we must have some starter variables.
+  .startWith(starters)
+
 })
 
+var source = Rx.Observable.combineLatest(streams)
 
-function notify(){
+function notify(msg){
+
   notifier.notify({
-    'title': 'My notification',
-    'message': 'on line!',
+    'title': 'your item is online!',
+    'message': msg,
     sound: true,
   });
 }
-Promise.any(checkers)
-.then(function() {
-  checkers.forEach(function(c){
-    // note: the one fullfilled won't call `onCancel()`
-    c.cancel();
-  })
-  notify();
-  setInterval(notify, 60*1000)
-})
-.catch(function(e) {console.log('err', e)})
+
+
+var notifyStream;
+
+source.subscribe(
+    function (results) {
+        if(notifyStream) notifyStream.dispose();
+
+        console.log('here in Next!!, total %d times on start!', carts.length+1);
+        let onlineItems = results.filter(function(result) {
+          return result.status
+        })
+        .map(function(result) {
+          return result.item
+        });
+
+        if(!onlineItems.length) return;
+        var msg = onlineItems.map(function(item) {return item.name})
+        .join("\n")
+
+        // frequently notify our user
+        // TODO: add some interaction allowing user to discard further notification
+        notifyStream = Rx.Observable.timer(0, ONE_MIN/2)
+        .subscribe(function(idx) {
+
+          notify(`${msg}\n(${idx+1} times)`);
+          
+        }, function() {}, function() {
+          console.log('notifyStream Completed')
+        })
+
+
+    },
+
+    function (err) {
+        console.log('Error: ' + err);
+    },
+    function () {
+        console.log('Completed');
+    });
